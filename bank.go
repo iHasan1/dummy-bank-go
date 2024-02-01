0ccca6a6a657b49634d3acad4e0d1a5bf66c3f25
Imran Hasan <imran.h94@gmail.com>
2024-02-01 18:27:45 +0500

    added error handling

--- bank.go
+++ bank.go
@@ -4,18 +4,28 @@ import (
 	"fmt"
 	"os"
 	"strconv" 
+	"errors"
 )
 
 //strconv helps with string conversion
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() float64{
-	data, _ := os.ReadFile(accountBalanceFile) // using underscore simply means telling go u dont want to use that returned value right now
-	balanceText := string(data)
-	balance, _ := strconv.ParseFloat(balanceText, 64)
+func getBalanceFromFile() (float64, error){
+	data, err := os.ReadFile(accountBalanceFile) // using underscore simply means telling go u dont want to use that returned value right now
 
-	return balance
+	if err != nil {
+		return 1000.0, errors.New("Failed to find file")
+	}
+
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+
+	if err != nil {
+		return 1000.0, errors.New("Failed to parse stored value")
+	}
+
+	return balance, nil
 }
 
 func writeBalanceToFile(balance float64) {
@@ -25,7 +35,13 @@ func writeBalanceToFile(balance float64) {
 }
 
 func main() {
-	var accountBalance = getBalanceFromFile()
+	var accountBalance, err = getBalanceFromFile()
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("--------")
+	}
 
 	fmt.Println("Welcome to Go Bank")
 
