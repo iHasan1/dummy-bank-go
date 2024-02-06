cbc19343050dda3f3295aed4ce6398337f7cd9cc
Imran Hasan <imran.h94@gmail.com>
2024-02-06 11:42:40 +0500

    modified read and write to file functions to make them more generic

--- bank.go
+++ bank.go
@@ -11,31 +11,31 @@ import (
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() (float64, error){
-	data, err := os.ReadFile(accountBalanceFile) // using underscore simply means telling go u dont want to use that returned value right now
+func getFloatFromFile(fileName string) (float64, error){
+	data, err := os.ReadFile(fileName) // using underscore simply means telling go u dont want to use that returned value right now
 
 	if err != nil {
 		return 1000.0, errors.New("failed to find file")
 	}
 
-	balanceText := string(data)
-	balance, err := strconv.ParseFloat(balanceText, 64)
+	fileText := string(data)
+	dataStr, err := strconv.ParseFloat(fileText, 64)
 
 	if err != nil {
 		return 1000.0, errors.New("failed to parse stored value")
 	}
 
-	return balance, nil
+	return dataStr, nil
 }
 
-func writeBalanceToFile(balance float64) {
-	balanceText := fmt.Sprint(balance)
-	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644) //0644 is a file permissions notation which means owner can read write and others can only read
+func writeFloatToFile(value float64, fileName string) {
+	valueText := fmt.Sprint(value)
+	os.WriteFile(fileName, []byte(valueText), 0644) //0644 is a file permissions notation which means owner can read write and others can only read
 
 }
 
 func main() {
-	var accountBalance, err = getBalanceFromFile()
+	var accountBalance, err = getFloatFromFile(accountBalanceFile)
 
 	if err != nil {
 		fmt.Println("ERROR")
@@ -69,7 +69,7 @@ func main() {
 
 			accountBalance += depositAmount
 			fmt.Println("Balance updated! New amount:", accountBalance)
-			writeBalanceToFile((accountBalance))
+			writeFloatToFile(accountBalance, accountBalanceFile)
 		case 3:
 			fmt.Print("Your withdrawal: ")
 			var withdrawalAmount float64
@@ -89,7 +89,7 @@ func main() {
 
 			accountBalance -= withdrawalAmount
 			fmt.Println("Balance updated! New amount:", accountBalance)
-			writeBalanceToFile(accountBalance)
+			writeFloatToFile(accountBalance, accountBalanceFile)
 		default:
 			fmt.Println("Goodbye!")
 			fmt.Println("Thanks for using our Bank.")
