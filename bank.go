8054ab53e2abe4b5cfd018bfec7007085717fb3a
Imran Hasan <imran.h94@gmail.com>
2024-02-06 11:46:04 +0500

    moved read write functions to separate file and package

--- bank.go
+++ bank.go
@@ -2,37 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv" 
-	"errors"
 )
 
 //strconv helps with string conversion
 
 const accountBalanceFile = "balance.txt"
-
-func getFloatFromFile(fileName string) (float64, error){
-	data, err := os.ReadFile(fileName) // using underscore simply means telling go u dont want to use that returned value right now
-
-	if err != nil {
-		return 1000.0, errors.New("failed to find file")
-	}
-
-	fileText := string(data)
-	dataStr, err := strconv.ParseFloat(fileText, 64)
-
-	if err != nil {
-		return 1000.0, errors.New("failed to parse stored value")
-	}
-
-	return dataStr, nil
-}
-
-func writeFloatToFile(value float64, fileName string) {
-	valueText := fmt.Sprint(value)
-	os.WriteFile(fileName, []byte(valueText), 0644) //0644 is a file permissions notation which means owner can read write and others can only read
-
-}
 
 func main() {
 	var accountBalance, err = getFloatFromFile(accountBalanceFile)
